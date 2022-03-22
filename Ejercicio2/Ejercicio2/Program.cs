754104b0d46043c84cc7d17f845f35598f242fc3
Jennifer <jenni.guglielmino@gmail.com>
2022-03-22 18:28:57 -0300

    ejercicio 2

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 2";
+            int numero;
+            double resultadoCuadrado;
+            double resultadoCubo;
+            Console.Write("Ingrese un numero: ");
+            int.TryParse(Console.ReadLine(), out numero);
+            do
+            {
+                Console.WriteLine("ERROR. ¡Reingresar numero!");
+                Console.Write("Ingrese un numero: ");
+                int.TryParse(Console.ReadLine(), out numero);
+            } while ((numero < 0));
+
+            resultadoCuadrado = Math.Pow(numero, 2);
+            resultadoCubo = Math.Pow(numero, 3);
+            Console.WriteLine($"El cuadrado del numero ingresado es {resultadoCuadrado}, el cubo {resultadoCubo}");
+            Console.ReadKey();
+        }
+    }
+}
